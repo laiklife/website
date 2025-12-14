16c203fb4eb80d5e0a85c05c1ef1194c52b12553
laiklife <github@laiklife.fr>
2025-12-15 00:12:14 +0100

    Update photos.ts

--- src/data/photos.ts
+++ src/data/photos.ts
@@ -16,8 +16,8 @@ export const TAGS = [
 ];
 
 export const PHOTOS: Photo[] = [
-  { src: "/photos/sample1.jpg", alt: "Sample 1", tags: ["nature"] },
-  { src: "/photos/sample2.jpg", alt: "Sample 2", tags: ["city", "architecture"] },
-  { src: "/photos/sample3.jpg", alt: "Sample 3", tags: ["portrait"] },
-  { src: "/photos/sample4.jpg", alt: "Sample 4", tags: ["black & white", "street"] }
+  { src: "/photos/sample1.jpeg", alt: "Sample 1", tags: ["nature"] },
+  { src: "/photos/sample2.jpeg", alt: "Sample 2", tags: ["city", "architecture"] },
+  { src: "/photos/sample3.jpeg", alt: "Sample 3", tags: ["portrait"] },
+  { src: "/photos/sample4.jpeg", alt: "Sample 4", tags: ["black & white", "street"] }
 ];
